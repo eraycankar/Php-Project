77124cc4ff94cb60cc5a16acae2814204d9a856a
eraycankar <56090358+eraycankar@users.noreply.github.com>
2022-01-11 21:08:47 +0300

    Upload project

---
+++ ErayCankarProject/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+
+<html>
+
+  <body>
+	<h1>
+		PHP Final Project
+	</h1>
+	<h3>
+		Eray Çankar
+	</h3>
+	<h3>
+		16290017
+	</h3>
+	<p> Inventory ve Sales veri tabanı tabloları oluşturuldu ve önyüze table eklendi.</p>
+	<p>	
+		<a href="http://localhost/ErayCankarProject/tableedit.php#page=inventory">Buradan</a>  projeye ulaşabilirsiniz.		
+	</p>
+	<p>
+		Ürün ekleme, Ürün miktarının güncellenmesi, ürün fiyatının güncellenmesi ve yapılan toplam satışın gösterilmesi, ürün satışının eklenmesi fonksiyonları var ve envanter tablosunun çıktısının alınabilmesi için print butonu eklenmiştir.  
+	</p>
+  </body>
+
+</html>
+
+
+
